62cd9df2f5f4d91541375576c669c177e6617969
Alessandro X. Moraes <alessandromoraes267@gmail.com>
2025-07-17 15:53:38 -0300

    Update creating_the_drive.py

--- Automation/Libraries/Selenium/creating_the_drive.py
+++ Automation/Libraries/Selenium/creating_the_drive.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option(name="detach", value=True)
@@ -11,18 +16,29 @@ service = Service(executable_path=path)
 driver = webdriver.Chrome(options=options, service=service)
 driver.get(website)
 
-containers = driver.find_elements(by="xpath", value='//a[@class="tab_item app_impression_tracked"]')
+wait= WebDriverWait(driver, 10)
+section = wait.until(EC.presence_of_element_located((By.ID, "tab_newreleases_content")))
+containers = section.find_elements(By.CLASS_NAME, "tab_item")
 
+game_titles = []
+game_genres = []
+game_links = []
 
 for container in containers:
-    game_title = container.find_element(by="xpath", value='./div [@class="tab_item_content"]/div[@class="tab_item_name"]').text
-    game_genre = container.find_element(by="xpath", value='./div [@class="tab_item_content"]/div[@class="tab_item_details"]').text
-    game_link = container.find_element(by="xpath", value='//a[@class="tab_item app_impression_tracked"]').get_attribute("href")
+    game_title = container.find_element(By.CLASS_NAME, "tab_item_name").text
+    game_genre = container.find_element(By.CLASS_NAME, "tab_item_details").text
+    game_link = container.get_attribute("href")
+    game_titles.append(game_title)
+    game_genres.append(game_genre)
+    game_links.append(game_link)
+    print(f"Obtido: {game_title}")
 
 
+steam_dict = {'Game Title': game_titles,
+        'Game Genre': game_genres,
+        'Game Link': game_links}
 
+df_steam = pd.DataFrame(steam_dict)
+df_steam.to_csv('new_trending.csv', index=False)
 
-
-# //a[@class="tab_item app_impression_tracked"]
-# //a[@class="tab_item app_impression_tracked"]/div [@class="tab_item_content"]/div[@class="tab_item_details"]
-# '//a[@class="tab_item app_impression_tracked"]/div [@class="tab_item_content"]/div[@class="tab_item_name"]'
+driver.quit()
